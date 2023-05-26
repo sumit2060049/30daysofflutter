43424158ecd104f2554f50e1f5dc24f48821264c
sumit2060049 <100397235+sumit2060049@users.noreply.github.com>
2023-05-27 01:53:00 +0530

    Day13complete

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:flutter_application_1/models/catalog.dart';
 
 import '../widgets/drawer.dart';
 import '../widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   //const HomePage({super.key});
   final int days = 30;
+
   final String name = "Sumit";
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    //print(catalogJson);
+    var decodedData = jsonDecode(catalogJson);
+    //print(decodedData);
+    var productsData = decodedData["products"];
+    //print(productsData);
+  }
 
   @override
   Widget build(BuildContext context) {
